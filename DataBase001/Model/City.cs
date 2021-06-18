ab86f612e6a9f18a723ad1d52d233fff9ccc047d
AzodTM <AzodTM@gmail.com>
2021-06-18 14:45:25 +0500

    Update DataBase001/Model/City.cs
    Co-authored-by: Evgeny Belousov <elessar.perm@gmail.com>

--- DataBase001/Model/City.cs
+++ DataBase001/Model/City.cs
@@ -6,7 +6,7 @@ namespace DataBase001.Model
 {
     public class City : Entity
     {
-        public int population;
+        public readonly uint Population;
         public City(string name,int population = 1000 ) : base(name)
         {
             this.population = population;
